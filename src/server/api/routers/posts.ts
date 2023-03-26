43bef7b56d6706f64d94314e86258512d983fe99
Jose Luis Bautista <jose.luis.bautista@obf.ateneo.edu>
2023-03-27 02:08:12 +0800

    loading states added

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,45 +1,9 @@
-import clerkClient from "@clerk/clerk-sdk-node";
 import { z } from "zod";
-import type { User } from "@clerk/nextjs/dist/api";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { TRPCError } from "@trpc/server";
 
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(async ({ ctx }) => {
-    const posts = await ctx.prisma.post.findMany({
-      take: 100,
-    });
-
-    const users = (
-      await clerkClient.users.getUserList({
-        userId: posts.map((post) => post.authorId),
-        limit: 100,
-      })
-    ).map(filterUserForClient);
-
-    return posts.map((post) => {
-      const author = users.find((user) => user.id === post.authorId);
-
-      if (!author)
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Author not found",
-        });
-
-      return {
-        post,
-        author,
-      };
-    });
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.post.findMany();
   }),
 });
